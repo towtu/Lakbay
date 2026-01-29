047ca8795349b989559201f47a0241d19f4579ea
towtu <fatowtu123@gmail.com>
2026-01-30 00:24:55 +0800

    web tweak pt 3

--- travel_planner/build/web/flutter_bootstrap.js
+++ travel_planner/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"db373eb85a06eb3d706af362a45d42d0972f4b
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "839248381"
+    serviceWorkerVersion: "4150013320"
   }
 });
